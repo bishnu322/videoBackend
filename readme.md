c43c322b29ef67a16889b0fafbbc33020b9be5a4
bishnu_kohar <koharbishnu322@gmail.com>
2025-07-11 15:29:31 +0545

    all data

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # video backend
 
 this is video backend with javascript
+
+bcrypt -> helps to hash password
+jsonwebtoken -> JWT
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -47,4 +49,12 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  await bcrypt.compare(password, this.password);
+};
 export const User = mongoose.model("User", userSchema);
